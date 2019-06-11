a109cba60e71fbd9f551d4922389c42996e27a5f
Ryan Mehta <ryan.mehta@gmail.com>
2019-06-10 17:06:21 -0700

    add list stuff

--- src/reducers/notesReducer.test.js
+++ src/reducers/notesReducer.test.js
@@ -1,5 +1,5 @@
 import reducer from './notesReducer';
-import { newNote, NEW_NOTE_PENDING, NEW_NOTE } from '../actions/notesActions';
+import { newNote, NEW_NOTE_PENDING, NEW_NOTE, FETCH_NOTES_PENDING, FETCH_NOTES } from '../actions/notesActions';
 
 describe('notes reducer', () => {
   it('handles the NEW_NOTE_PENDING action', () => {
@@ -28,6 +28,37 @@ describe('notes reducer', () => {
         title: 'hi',
         body: 'bye'
       }
+    });
+
+    expect(newState).toEqual({
+      loading: false,
+      list: [{ title: 'hi', body: 'bye' }]
+    });
+  });
+
+  it('handles the FETCH_NOTES_PENDING action', () => {
+    const initialState = {
+      loading: false,
+      list: []
+    };
+    const newState = reducer(initialState, {
+      type: FETCH_NOTES_PENDING
+    });
+
+    expect(newState).toEqual({
+      loading: true,
+      list: []
+    });
+  });
+
+  it('handles the FETCH_NOTES action', () => {
+    const initialState = {
+      loading: true,
+      list: []
+    };
+    const newState = reducer(initialState, {
+      type: FETCH_NOTES,
+      payload: [{ title: 'hi', body: 'bye' }]
     });
 
     expect(newState).toEqual({
